4dfb7b05033cb5cdcb75a0a56a402c3c26b48646
tsieyy <tsieyy@qq.com>
2023-04-30 07:31:04 +0800

    add stream dataset

--- Main/_03Training/_02PipeDatasetLoader.py
+++ Main/_03Training/_02PipeDatasetLoader.py
@@ -7,6 +7,8 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import transforms
+from threading import Thread
+import time
 
 InputImgSize=(128,128)
 # 训练过程图片的变换
@@ -106,8 +108,58 @@ class PredictDataset(Dataset):
 		random.seed(seed)
 		img = self.img_transform(img)
 		return img, self.sample_folders[item]
+	
+# 视频流dataset处理
+class StreamDataset(Dataset):
+	def __init__(self, sources='streams.txt', transform=PredictImgTransform) -> None:
+		# 判断是否有多个数据源
+		if os.path.isfile(sources):
+			with open(sources, 'r') as f:
+				sources = [x.strip() for x in f.read().strip().splitlines() if len(x.strip())]
+		else:
+			sources = [sources]
 
+		n = len(sources)
+		self.imgs = [None] * n
+		self.sources = [x for x in sources]
+		
+		for i, s in enumerate(sources):
+			# 开启线程从视频流读数据
+			print(f'{i + 1}/{n}: {s}... ', end='')
+			url = eval(s) if s.isnumeric() else s
+			cap = cv2.VideoCapture(url)
+			assert cap.isOpened(), f'Failed to open {s}'
+			w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+			h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+			self.fps = cap.get(cv2.CAP_PROP_FPS) % 100
 
+			_, self.imgs[i] = cap.read()  # guarantee first frame
+			thread = Thread(target=self.update, args=([i, cap]), daemon=True)
+			print(f' success ({w}x{h} at {self.fps:.2f} FPS).')
+			thread.start()
+	def update(self, index, cap):
+        # Read next stream frame in a daemon thread
+		n = 0
+		while cap.isOpened():
+			n += 1
+            # _, self.imgs[index] = cap.read()
+			cap.grab()
+			if n == 4:  # read every 4th frame
+				success, im = cap.retrieve()
+				self.imgs[index] = im if success else self.imgs[index] * 0
+				n = 0
+				time.sleep(1 / self.fps)  # wait time
+
+	def __len__(self):
+		return len(self.imgs)
+
+	def __getitem__(self, index):
+			img_ = self.imgs[index]
+			# img = Image.open(img_path)
+			seed = np.random.randint(2147483647)
+			random.seed(seed)
+			img = self.img_transform(img_)
+			return img, img_
 
 
 
